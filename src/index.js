c5d601b218349a9c069963987d09bde6c9a02e29
Natallia Lanina <natalielanina@yahoo.com>
2021-08-11 19:42:53 +0300

    docs: morse-decoder test

--- src/index.js
+++ src/index.js
@@ -38,9 +38,20 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    const a = expr.match(/.{10}/g).toString();
+    const arr = a.replace(/\*{10}/g, ' ').replace(/10/g, '.').replace(/11/g, '-').replace(/0/g, '').split(',');
+    const result = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+     if (arr[i] == ' ') {
+          result.push(' ');
+      } else {
+          result.push(MORSE_TABLE[arr[i]]);
+     }
+   }
+  return result.join('');
+  };
 
 module.exports = {
     decode
-}
+};
